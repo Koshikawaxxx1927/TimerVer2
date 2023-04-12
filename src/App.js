bccf65d7099585140c767e86579eed14b5f62a1c
Koshikawaxxx1927 <ebi1927@icloud.com>
2023-04-12 22:53:20 +0900

    Input.js and Button.js are added and arranged.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Input from "./components/Input";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import AddButton from "./components/AddButton";
 
 function App() {
     return (
         <div className="App">
-            <Input />
+            <div className="container-sm">
+                <AddButton />
+                <Input />
+                <Input />
+            </div>
         </div>
     );
 }
